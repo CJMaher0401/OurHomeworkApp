6343f0766bde0d9d9107a4905cca499a2cf46edd
Connor <therealconnor.maher@gmail.com>
2023-12-02 16:47:01 -0700

    Made changes to certain screens and started implmenting functionality in regards to maneuvering around the app. So making sure all buttons lead to the correct layouts and when within a nmew layout you can maneuver to other layouts or go back to your orginal layout you were in

--- app/src/main/java/com/example/ourhomeworkapp/MainActivity.kt
+++ app/src/main/java/com/example/ourhomeworkapp/MainActivity.kt
@@ -1,34 +1,139 @@
 package com.example.ourhomeworkapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
+import java.util.Stack
 
 
 class MainActivity : ComponentActivity() {
-    //created my own personal branch
+
+    private val layoutStack: Stack<Int> = Stack()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homescreen_layout)
 
-        val homeProfileButton : Button = findViewById(R.id.profileButton)
-        val addHWbutton : ImageButton = findViewById(R.id.addHWbutton)
-
-        homeProfileButton.setOnClickListener{
-            profileScreen()
+        findViewById<Button>(R.id.profileButton).setOnClickListener{
+            inflateLayout(R.layout.profilescreen_layout)
         }
-
-        addHWbutton.setOnClickListener{
-            addHWScreen()
+        findViewById<ImageButton>(R.id.addHWbutton).setOnClickListener{
+            inflateLayout(R.layout.addhomeworkscreen_layout)
+        }
+        findViewById<Button>(R.id.homeScreenButton).setOnClickListener{
+            inflateLayout(R.layout.homescreen_layout)
+        }
+        findViewById<Button>(R.id.homeScreenMyHWButton).setOnClickListener{
+            inflateLayout(R.layout.currentupcominghw_layout)
+        }
+        findViewById<Button>(R.id.homeScreenMyProfileButton).setOnClickListener{
+            inflateLayout(R.layout.profilescreen_layout)
         }
     }
-    private fun profileScreen()
+
+    private fun inflateLayout(layoutResID: Int)
     {
-        setContentView(R.layout.profilescreen_layout)
-    }
-    private fun addHWScreen()
-    {
-        setContentView(R.layout.addhomeworkscreen_layout)
+
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(layoutResID, null)
+
+        setContentView(layout)
+
+        when (layoutResID) {
+            R.layout.homescreen_layout -> {
+                findViewById<Button>(R.id.profileButton).setOnClickListener{
+                    inflateLayout(R.layout.profilescreen_layout)
+                }
+                findViewById<ImageButton>(R.id.addHWbutton).setOnClickListener{
+                    inflateLayout(R.layout.addhomeworkscreen_layout)
+                }
+                findViewById<Button>(R.id.homeScreenButton).setOnClickListener{
+                    inflateLayout(R.layout.homescreen_layout)
+                }
+                findViewById<Button>(R.id.homeScreenMyHWButton).setOnClickListener{
+                    inflateLayout(R.layout.currentupcominghw_layout)
+                }
+                findViewById<Button>(R.id.homeScreenMyProfileButton).setOnClickListener{
+                    inflateLayout(R.layout.profilescreen_layout)
+                }
+            }
+            R.layout.profilescreen_layout -> {
+                findViewById<Button>(R.id.profileHomeButton).setOnClickListener{
+                    inflateLayout(R.layout.homescreen_layout)
+                }
+                findViewById<Button>(R.id.profileMyHWButton).setOnClickListener{
+                    inflateLayout(R.layout.currentupcominghw_layout)
+                }
+                findViewById<Button>(R.id.profileMyProfileButton).setOnClickListener{
+                    inflateLayout(R.layout.profilescreen_layout)
+                }
+            }
+            R.layout.addhomeworkscreen_layout -> {
+                findViewById<Button>(R.id.addHWcancelButton).setOnClickListener{
+                    inflateLayout(R.layout.homescreen_layout)
+                }
+                findViewById<Button>(R.id.addHWsaveButton).setOnClickListener{
+                    inflateLayout(R.layout.currentupcominghw_layout)
+                }
+                findViewById<EditText>(R.id.editClassDescText).setOnClickListener{
+                    inflateLayout(R.layout.yourcoursesscreen_layout)
+                }
+                findViewById<EditText>(R.id.editDueDateText).setOnClickListener{
+                    inflateLayout(R.layout.duedateselectionscreen_layout)
+                }
+                findViewById<EditText>(R.id.editReminderText).setOnClickListener{
+                    inflateLayout(R.layout.reminderdateselectionscreen_layout)
+                }
+            }
+            R.layout.yourcoursesscreen_layout -> {
+                findViewById<ImageButton>(R.id.returnToAddHWButton).setOnClickListener{
+                    inflateLayout(R.layout.addhomeworkscreen_layout)
+                }
+                findViewById<ImageButton>(R.id.addNewClassButton).setOnClickListener{
+                    inflateLayout(R.layout.coursecreationscreen_layout)
+                }
+            }
+            R.layout.coursecreationscreen_layout -> {
+                findViewById<Button>(R.id.coursecancelButton).setOnClickListener{
+                    inflateLayout(R.layout.yourcoursesscreen_layout)
+                }
+                findViewById<Button>(R.id.coursesaveButton).setOnClickListener{
+                    inflateLayout(R.layout.yourcoursesscreen_layout)
+                }
+            }
+            R.layout.duedateselectionscreen_layout -> {
+
+            }
+            R.layout.currentupcominghw_layout -> {
+                findViewById<Button>(R.id.curHWcancelButton).setOnClickListener{
+                    inflateLayout(R.layout.homescreen_layout)
+                }
+                findViewById<Button>(R.id.curHWaddHWbutton).setOnClickListener{
+                    inflateLayout(R.layout.addhomeworkscreen_layout)
+                }
+                findViewById<Button>(R.id.curHWButton).setOnClickListener{
+                    inflateLayout(R.layout.currentupcominghw_layout)
+                }
+                findViewById<Button>(R.id.compHWButton).setOnClickListener{
+                    inflateLayout(R.layout.completedhwscreen_layout)
+                }
+            }
+            R.layout.completedhwscreen_layout -> {
+                findViewById<Button>(R.id.compHWcancelButton).setOnClickListener{
+                    inflateLayout(R.layout.homescreen_layout)
+                }
+                findViewById<Button>(R.id.compHWaddHWbutton).setOnClickListener{
+                    inflateLayout(R.layout.addhomeworkscreen_layout)
+                }
+                findViewById<Button>(R.id.compCurHWButton).setOnClickListener{
+                    inflateLayout(R.layout.currentupcominghw_layout)
+                }
+                findViewById<Button>(R.id.compCompHWButton).setOnClickListener{
+                    inflateLayout(R.layout.completedhwscreen_layout)
+                }
+            }
+        }
     }
 }
