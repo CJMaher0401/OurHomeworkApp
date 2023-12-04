8908836a32f5e03625a7ea5d0d8d06c7ae258d12
Connor <therealconnor.maher@gmail.com>
2023-12-04 14:20:24 -0700

    Had to change my theme in androidmanifest to get a colorpicker to work add to add the git repo into my implmentation in build.gradle, edited coursecreation screen to include an invisible colorwheelpicker and add the function to have the color wheel pop up

--- app/src/main/java/com/example/ourhomeworkapp/MainActivity.kt
+++ app/src/main/java/com/example/ourhomeworkapp/MainActivity.kt
@@ -2,21 +2,31 @@ package com.example.ourhomeworkapp
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.ColorPickerView
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 import java.util.Stack
 
 
+
 class MainActivity : ComponentActivity() {
 
     private val layoutStack: Stack<Int> = Stack()
+    private lateinit var colorPickerView: ColorPickerView
+    private lateinit var colorEditText: EditText
+    private lateinit var colorPickerButton: Button
+    private var selectedColor: Int = Color.BLACK
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homescreen_layout)
@@ -36,6 +46,7 @@ class MainActivity : ComponentActivity() {
         findViewById<Button>(R.id.homeScreenMyProfileButton).setOnClickListener{
             inflateLayout(R.layout.profilescreen_layout)
         }
+
     }
 
     private fun inflateLayout(layoutResID: Int)
@@ -109,6 +120,9 @@ class MainActivity : ComponentActivity() {
                 findViewById<Button>(R.id.coursesaveButton).setOnClickListener{
                     inflateLayout(R.layout.yourcoursesscreen_layout)
                 }
+                findViewById<Button>(R.id.pickYourColorButton).setOnClickListener {
+                    showColorWheel()
+                }
             }
             R.layout.duedateselectionscreen_layout -> {
                 findViewById<Button>(R.id.dueDateCancelButton).setOnClickListener{
@@ -157,7 +171,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun showDatePicker(editText: EditText) {
+    private fun showDatePicker(editText: EditText)
+    {
         val currentDate = Calendar.getInstance()
         val year = currentDate.get(Calendar.YEAR)
         val month = currentDate.get(Calendar.MONTH)
@@ -169,22 +184,18 @@ class MainActivity : ComponentActivity() {
                 selectedDate.set(selectedYear, selectedMonth, selectedDay)
 
                 val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
-                editText.setText(dateFormat.format(selectedDate.time))
-            },
-            year,
-            month,
-            day
-        )
+                editText.setText(dateFormat.format(selectedDate.time)) }, year, month, day)
         datePickerDialog.show()
     }
 
-    private fun showDateAndTimePicker(editText: EditText) {
-        val currentDateTime = Calendar.getInstance()
-        val year = currentDateTime.get(Calendar.YEAR)
-        val month = currentDateTime.get(Calendar.MONTH)
-        val day = currentDateTime.get(Calendar.DAY_OF_MONTH)
-        val hour = currentDateTime.get(Calendar.HOUR_OF_DAY)
-        val minute = currentDateTime.get(Calendar.MINUTE)
+    private fun showDateAndTimePicker(editText: EditText)
+    {
+        val currentDateAndTime = Calendar.getInstance()
+        val year = currentDateAndTime.get(Calendar.YEAR)
+        val month = currentDateAndTime.get(Calendar.MONTH)
+        val day = currentDateAndTime.get(Calendar.DAY_OF_MONTH)
+        val hour = currentDateAndTime.get(Calendar.HOUR_OF_DAY)
+        val minute = currentDateAndTime.get(Calendar.MINUTE)
 
         val datePickerDialog = DatePickerDialog(
             this,
@@ -195,19 +206,34 @@ class MainActivity : ComponentActivity() {
                         selectedDateTime.set(selectedYear, selectedMonth, selectedDay, selectedHour, selectedMinute)
 
                         val dateFormat = SimpleDateFormat("MMMM dd, yyyy 'at' h:mm a", Locale.getDefault())
-                        editText.setText(dateFormat.format(selectedDateTime.time))
-                    },
-                    hour,
-                    minute,
-                    false
-                )
-
-                timePickerDialog.show()
-            },
-            year,
-            month,
-            day
-        )
+                        editText.setText(dateFormat.format(selectedDateTime.time)) }, hour, minute, false)
+                timePickerDialog.show() }, year, month, day)
         datePickerDialog.show()
+    }
+
+    private fun showColorWheel()
+    {
+        if (!::colorEditText.isInitialized)
+        {
+            colorEditText = findViewById(R.id.nameOfCourseText)
+        }
+
+        if (!::colorPickerView.isInitialized)
+        {
+            colorPickerView = findViewById(R.id.colorPickerView)
+        }
+
+        colorPickerView.visibility = View.VISIBLE
+
+        ColorPickerDialog.Builder(this).setTitle("Pick a color for your course!").setPreferenceName("MyColorPickerDialog")
+            .setPositiveButton("Save", ColorEnvelopeListener { envelope, _ -> colorEditText.setTextColor(envelope.color)
+                colorPickerView.visibility = View.GONE
+            })
+            .setNegativeButton("Cancel") { _, _ ->
+                colorPickerView.visibility = View.GONE
+            }
+            .attachAlphaSlideBar(true)
+            .attachBrightnessSlideBar(true)
+            .show()
     }
 }
